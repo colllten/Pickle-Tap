f5c88602db5f44147cff76adf3c3f319002dfa53
Colten Glover <coltenglover@yahoo.com>
2025-06-24 11:05:55 -0400

    Initial Commit

---
+++ PaddlePoints Watch App/PaddlePointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaddlePointsApp.swift
+//  PaddlePoints Watch App
+//
+//  Created by Colten Glover on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct PaddlePoints_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
