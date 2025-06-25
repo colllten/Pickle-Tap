ca59008b11b888b80cf25555c231843c9501b084
Colten Glover <coltenglover@yahoo.com>
2025-06-25 15:41:22 -0400

    Moved files; added Game model

---
+++ Pickle Tap Watch App/Models/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  Pickle Tap Watch App
+//
+//  Created by Colten Glover on 6/25/25.
+//
+
+import Foundation
+
+class Game {
+    var userPoints: UInt = 0
+    var opponentPoints: UInt = 0
+}
